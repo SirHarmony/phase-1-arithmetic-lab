80540365e785843aee209e218e7278de453e67d1
Harmony <harmonymukolwe@gmail.com>
2024-06-26 11:22:13 +0300

    Completes module

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+// Multiply
+const num1 = 31;
+const num2 = 2;
 
-// Write your code here
+const num3 = 24;
+const num4 = 5;
+
+const multiply = num1 * num2;
+
+const random = Math.ceil(Math.random()*10);
+
+const mod = num3 % num4;
+
+const max = Math.max(1,2,15,19,20);
